2b9afc06031a971665161875e543e106402f1d4a
Vadim Semenov <semenov@bimdance.io>
2024-04-09 18:54:42 +0300

    Add otus JS 1 part

---
+++ 1Lesson/script.js
@@ -0,0 +1,30 @@
+//Lesson 1
+// console.log('Hello World!');
+// alert('ddsffsf');
+// prompt('dfsfssdfsfd');
+// confirm('dfsfssdfsfd');
+
+//Lesson 1.3.1
+// const firstName = 'Vadim';
+// const lastName = 'Semenov';
+// console.log(firstName + ' ' + lastName);
+// console.log(`Hello ${firstName} ${lastName}!`);
+
+// const word = prompt("What is your name?");
+// console.log(word.length);
+
+//Lesson 1.3.2
+// const sum = 0.1 +0.2;
+// console.log(+sum.toFixed(3));
+
+// const miles = prompt(`Miles`);
+// console.log(`${miles*1.6} km`);
+
+//Lesson 1.3.3
+
+//Домашнее здание по разделу 1
+//1
+//2
+//3
+// const number = prompt("Enter your number");
+// document.write(number % 10 + Math.floor(number / 10 % 10) + Math.floor(number / 100 % 10));
